4bded154ad1ea1db407ee5ddda065e57e0df529d
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-12-18 04:05:11 +0600

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by A.f. Adib on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
